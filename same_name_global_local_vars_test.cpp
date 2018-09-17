cc892025f51bb24ee29d647f6ba1dceac8e9b60c
bgnMjr <bm@mj.com>
2018-09-17 12:39:18 +0200

    added access to global variables from local scope

--- same_name_global_local_vars_test.cpp
+++ same_name_global_local_vars_test.cpp
@@ -19,8 +19,17 @@ int main() {
 	int j = i; //Value of local variable i is used here
 	int str = 22; //Local str differs from the global one both in type and value
 
+  cout << "Local var i = " << i << endl; //returns 100
 	cout << "The value of j is " << j << endl; //returns 100
-	cout << "The local str variable is equal to: " << str << endl; //return 22
+	cout << "The local str variable is equal to: " << str << endl; //returns 22
+
+  /*
+   * Now testing access to global variable from local scope
+	 * using unary scope resolution operator ::
+	 */
+
+	cout << "Global var i = " << ::i << endl; //return 42
+	cout << "Global var str = " << ::str << endl; //returns Blah blah blah
 
 	return 0;
 }
